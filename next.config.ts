258adee2c6bd531359d61a9edc82807257ed7bec
authexinc <dev@authex.co>
2026-03-01 16:00:56 -0700

    init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com", 
+        protocol: "https"
+      },
+      {
+        hostname: "avatars.githubusercontent.com", 
+        protocol: "https"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
